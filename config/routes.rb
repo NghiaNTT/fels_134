68b60325285337a18213c580a1a153ab29beae4b
huongnguyenmta <huongnt0809@gmail.com>
2016-05-17 19:54:03 +0700

    Signup
    sign up
    
    second change login and log out

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   get "help" => "static_pages#help"
   get "about" => "static_pages#about"
   get "contact" => "static_pages#contact"
+  get "signup" => "users#new"
+  resources :users, only: [:new, :create, :show]
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
 end
