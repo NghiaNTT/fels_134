7053649f6154efcda8c72a6eb6a459ee15adfb64
NghiaNTT <nguyen.thi.trong.nghia@framgia.com>
2016-06-20 13:25:20 +0700

    admin-edit-a-categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController < ApplicationController
-  before_action :logged_in_user, :verify_admin_user
+  before_action :logged_in_user, :verify_admin_user, only: [:index, :destroy, :index]
 
   def index
     @categories = Category.paginate page: params[:page]
@@ -8,4 +8,21 @@ class Admin::CategoriesController < ApplicationController
   def show
     @category = Category.find params[:id]
   end
+
+  def destroy
+    Category.find(params[:id]).destroy
+    flash[:success] = t "flash_message.delete_success"
+    redirect_to admin_categories_url
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      flash[:success] = t"edit_category.edit_success"
+      redirect_to @category
+    else
+      render :edit
+    end
+  end
+
 end
