94649b708b984ae17a3c205cd97d54cc5d418fcc
NghiaNTT <nguyen.thi.trong.nghia@framgia.com>
2016-06-28 09:29:43 +0700

    result lesson

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,7 @@
 class Answer < ActiveRecord::Base
   belongs_to :word
+  has_many :lesson_words, dependent: :destroy
+
   scope :eager_load_by_ids, ->(ids) do
     eager_load(word: :answers).where id: ids
   end
