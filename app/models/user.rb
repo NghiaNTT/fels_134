21948d2667f939c34053a296f0d795e4d5fa6052
NghiaNTT <nguyen.thi.trong.nghia@framgia.com>
2016-06-28 13:44:28 +0700

    following user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
     foreign_key: :following_id, dependent: :destroy
   has_many :lessons, dependent: :destroy
   has_many :following, through: :active_relationships, source: :following
-  has_many :follower, through: :passive_relationships, source: :follower
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token
   before_save {self.email = email.downcase}
@@ -51,5 +51,17 @@ class User < ActiveRecord::Base
 
   def is_user? user
     self == user
+  end
+
+  def follow other_user
+    active_relationships.create following_id: other_user.id
+  end
+
+  def unfollow other_user
+    active_relationships.find_by(following_id: other_user.id).destroy
+  end
+
+  def following? other_user
+    following.include? other_user
   end
 end
